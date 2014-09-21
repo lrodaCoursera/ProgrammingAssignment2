a722bb95b5840c43727265d16dd419b556e232cc
lrodaCoursera <lroda73@hotmail.com>
2014-09-22 01:27:55 +0200

    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+# a list will of functions will be generated: getCache returns the stored cache, 
+# getMatrix returns the matrix for which the inverse was calculated
+# setMatrix stores a new matrix 
+# setInvere stores the new inverse calculation
+# hasChanged tells if the Matrix has changed since the last updating of the inverse
 
+        i <- NULL       
+        changed <- FALSE
+        getCache <- function() i
+        getMatrix <- function() x
+        setMatrix <- function(y) {
+                x <<- y
+                changed <- TRUE
+        }
+        setInverse <- function(y) {
+                i <<- y
+                changed <- FALSE
+        }
+        hasChanged <- function() changed
+        list(getCache = getCache, getMatrix = getMatrix, setMatrix = setMatrix, setInverse = setInverse, hasChanged = hasChanged)
 }
 
 
@@ -12,4 +31,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ # if the cached inverse is not NULL yet and the matrix has not changed since last inverse value storing then we return cached value      
+        if ((x$hasChanged==FALSE)&&(!is.null(x$getCache)) {
+                message("getting cached data")
+                return(x$getCache)
+                
+        }
+        data <- x$getMatrix
+        i <- solve(data)
+        x$setInverse(i)
+        i
 }
