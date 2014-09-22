8c73f890b85928063b9c598796e1bd042da40e8f
lrodaCoursera <lroda73@hotmail.com>
2014-09-22 02:23:13 +0200

    Version pushed after deadline

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-# a list will of functions will be generated: getCache returns the stored cache, 
+# a list of functions will be generated: 
+# getCache returns the stored cache, 
 # getMatrix returns the matrix for which the inverse was calculated
 # setMatrix stores a new matrix 
-# setInvere stores the new inverse calculation
+# setInverse stores the new inverse calculation
 # hasChanged tells if the Matrix has changed since the last updating of the inverse
 
         i <- NULL       
@@ -16,11 +17,11 @@ makeCacheMatrix <- function(x = matrix()) {
         getMatrix <- function() x
         setMatrix <- function(y) {
                 x <<- y
-                changed <- TRUE
+                changed <<- TRUE
         }
         setInverse <- function(y) {
                 i <<- y
-                changed <- FALSE
+                changed <<- FALSE
         }
         hasChanged <- function() changed
         list(getCache = getCache, getMatrix = getMatrix, setMatrix = setMatrix, setInverse = setInverse, hasChanged = hasChanged)
@@ -31,13 +32,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
- # if the cached inverse is not NULL yet and the matrix has not changed since last inverse value storing then we return cached value      
-        if ((x$hasChanged==FALSE)&&(!is.null(x$getCache)) {
+        # if the cached inverse is not NULL 
+        # and the matrix has not changed since last inverse value storing 
+        # then we return cached value 
+        # otherwise we calculate the inverse of the matrix currently stored in X and we store the result
+        # as well as returning this new value
+        
+        if ((x$hasChanged() == FALSE) && (!is.null(x$getCache()))) {
                 message("getting cached data")
-                return(x$getCache)
+                return(x$getCache())
                 
         }
-        data <- x$getMatrix
+        data <- x$getMatrix()
         i <- solve(data)
         x$setInverse(i)
         i
